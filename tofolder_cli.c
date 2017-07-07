f680fc0eff4674d3d301698722d9d48cce892189
naens <naens@gmx.com>
2017-07-07 13:11:16 +0300

    tofolder_cli: list files and directories containing the lcs

--- tofolder_cli.c
+++ tofolder_cli.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
+#include <dirent.h>
 
 #include <pedit.h>
 
@@ -13,30 +15,58 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  uint32_t *strs32[argc - 1];
-  for (int i = 0; i < argc - 1; i++)
+  printf("input strings:\n");
+  int ndirs = argc - 1;
+  uint32_t *strs32[ndirs];
+  for (int i = 0; i < ndirs; i++)
   {
-//    printf("i=%d\n", i);
-    uint8_t *utf8str = argv[i + 1];
-//    printf("utf8str=%s\n", utf8str);
-    int sz = strlen(utf8str);
-    printf("sz=%d\n", sz);
-    int len8 = utf8strlen(utf8str);
-    printf("len8=%d\n", len8);
-//    printf("sz=%d chars=", sz);
-//    for (int j = 0; j < sz; j++)
-//      printf("%02x ", utf8str[j]);
-//    printf("\n");
+    char *fn = basename(argv[i + 1]);
+    int len8 = utf8strlen(fn);
+    int fnlen = strlen(fn);
+    printf("    \"%s\"\n", fn);
 
-    strs32[i] = malloc(sizeof (uint32_t) * len8);
-    str_utf8_to_ucs32(utf8str, strs32[i]);
-
-    int len32 = ucs32strlen(strs32[i]);
-    printf("len32=%d chars=", len32);
-    for (int j = 0; j < len32; j++)
-      printf("%02x ", strs32[i][j]);
-    printf("\n");
+    strs32[i] = malloc(sizeof (uint32_t) * (len8 + 1));
+    str_utf8_to_ucs32(fn, strs32[i]);
   }
+  char *dir = dirname(argv[1]);
+  int dirlen = strlen(dir);
+  printf("dir: \"%s\"\n", dir);
+
+  struct gst *gst = gst_new();
+
+  for (int i = 0; i < ndirs; i++)
+    gst_add_string(gst, strs32[i]);
+
+  int ls_cnt;
+  uint32_t **lss;
+  gst_get_longest_strings(gst, &ls_cnt, &lss);
+
+  if (ls_cnt == 0)
+  {
+    printf("no longest string found\n");
+    gst_free(gst);
+    return 0;
+  }
+  
+  char lcs[0x1000];
+  str_ucs32_to_utf8(lss[0], lcs);
+  printf("found longest string: \"%s\"\n", lcs);
+  for (int i = 0; i < ls_cnt; i++)
+    free(lss[i]);
+  gst_free(gst);
+
+  /* list of files containing the lcs substring */
+  DIR *d = opendir(dir);
+  struct dirent *dirent;
+  if (!d)
+    return -1;
+
+  while ((dirent = readdir(d)) != NULL)
+  {
+    if (strstr(dirent->d_name, lcs) != NULL)
+      printf("%s\n", dirent->d_name);
+  }
+  closedir(d);
 
   return 0;
 }
